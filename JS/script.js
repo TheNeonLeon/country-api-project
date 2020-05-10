d7a2b9df712396865bf2bae561374dd6962ef2aa
Jensengbg-Leon-Listo <leon.listo@hotmail.com>
2020-05-10 16:39:29 +0200

    upd

--- JS/script.js
+++ JS/script.js
@@ -11,3 +11,15 @@ document.getElementById('show-btn').addEventListener('click', loadData => {
         document.getElementById('list').innerHTML = html;
     });
 });
+//make every list show more info when clicked
+document.querySelector("li").addEventListener("click", onClick => {
+    console.log("test");
+});
+//iterate through countries and filter by search query
+document.querySelector(".search-btn").addEventListener('click', filterCountry => {
+    let ul = document.getElementById("list");
+    let li = ul.getElementsByTagName('li');
+    for (i = 0; i < li.length; i++) {
+
+    }
+})

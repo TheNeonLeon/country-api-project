491f65e9060e507d96a296d80005ee2cba480402
Le0n <leon.listo@hotmail.com>
2021-02-21 16:31:43 +0100

    upd

--- JS/index.js
+++ JS/index.js
@@ -14,45 +14,9 @@ window.addEventListener('load', loadData => {
             </li>
             `;
             console.log(data);
-            function filterCountry(){
-            let filterValue = document.getElementById('input-field').value.toUpperCase();
-            let li = document.getElementsByTagName('li');
-
-            for(let i = 0; i < li.length; i++) {
-                html += element[i]
-              if (li.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
-                li[i].style.display = '';
-            }
-          }
-        }
-        filterCountry();
-        });
+            
         document.getElementById('list').innerHTML = html;
         
-    });
-    
+     });
+  });
 });
-
-//make every list show more info when clicked
-/*document.getElementsByTagName('li').addEventListener("click", onClick => {
-
-});*/
-//iterate through countries and filter by search query
-
-   
-/*function filterCountry(){
-   let filterValue = document.getElementById('input-field').value.toUpperCase();
-   let wrapper = document.getElementById('list');
-   let li = wrapper.getElementsByTagName('li');
-
-   for (i = 0; i < li.length; i++) {
-    let list = li[i].getElementsByTagName('li')[0];
-    if (li.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
-        li[i].style.display = '';
-    }else {
-        li[i].style.display = 'none';
-    }
-  }
-}
-let filterInput = document.getElementById('input-field');
-filterInput.addEventListener('keyup', filterCountry());*/
